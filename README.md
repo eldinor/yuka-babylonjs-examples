492c5b5f0757293a3e82e70bb027f95c80b3cbc7
Roland Csibrei <roland.csibrei@nascor.tech>
2022-01-25 19:52:54 +0100

    Project directory structure and steering examples

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # yuka-babylon-examples
+
 Yuka Game AI + 3D rendering with Babylon.js
+
+# Installation
+
+# VS Code extensions to install
+
+https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode
+
+https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer
---
+++ examples/js/misc/savegame/src/CustomEntity.js
@@ -0,0 +1,57 @@
+/**
+ * @author Mugen87 / https://github.com/Mugen87
+ */
+
+import { GameEntity } from "../../../../build/yuka.module.js";
+
+class CustomEntity extends GameEntity {
+  constructor() {
+    super();
+
+    this.name = "target";
+    this.currentTime = 0;
+    this.endTime = 0;
+  }
+
+  update(delta) {
+    this.currentTime += delta;
+
+    if (this.currentTime >= this.endTime) {
+      this.generatePosition();
+    }
+
+    return super.update(delta);
+  }
+
+  generatePosition() {
+    const radius = 2;
+    const phi = Math.acos(2 * Math.random() - 1);
+    const theta = Math.random() * Math.PI * 2;
+
+    this.position.fromSpherical(radius, phi, theta);
+
+    this.endTime += 3; // 3s
+  }
+
+  toJSON() {
+    const json = super.toJSON();
+
+    json.currentTime = this.currentTime;
+    json.endTime = this.endTime;
+
+    console.log(json);
+
+    return json;
+  }
+
+  fromJSON(json) {
+    super.fromJSON(json);
+
+    this.currentTime = json.currentTime;
+    this.endTime = json.endTime;
+
+    return this;
+  }
+}
+
+export { CustomEntity };
