5d7b44f0869b22639e7f8b830e1b929be762d433
Roland Csibrei <roland.csibrei@nascor.tech>
2022-01-25 20:00:04 +0100

    Project directory structure and steering examples

--- examples/js/misc/savegame/src/CustomEntity.js
+++ examples/js/misc/savegame/src/CustomEntity.js
@@ -2,56 +2,56 @@
  * @author Mugen87 / https://github.com/Mugen87
  */
 
-import { GameEntity } from "../../../../build/yuka.module.js";
+import { GameEntity } from '../../../../../lib/yuka.module.js'
 
 class CustomEntity extends GameEntity {
   constructor() {
-    super();
+    super()
 
-    this.name = "target";
-    this.currentTime = 0;
-    this.endTime = 0;
+    this.name = 'target'
+    this.currentTime = 0
+    this.endTime = 0
   }
 
   update(delta) {
-    this.currentTime += delta;
+    this.currentTime += delta
 
     if (this.currentTime >= this.endTime) {
-      this.generatePosition();
+      this.generatePosition()
     }
 
-    return super.update(delta);
+    return super.update(delta)
   }
 
   generatePosition() {
-    const radius = 2;
-    const phi = Math.acos(2 * Math.random() - 1);
-    const theta = Math.random() * Math.PI * 2;
+    const radius = 2
+    const phi = Math.acos(2 * Math.random() - 1)
+    const theta = Math.random() * Math.PI * 2
 
-    this.position.fromSpherical(radius, phi, theta);
+    this.position.fromSpherical(radius, phi, theta)
 
-    this.endTime += 3; // 3s
+    this.endTime += 3 // 3s
   }
 
   toJSON() {
-    const json = super.toJSON();
+    const json = super.toJSON()
 
-    json.currentTime = this.currentTime;
-    json.endTime = this.endTime;
+    json.currentTime = this.currentTime
+    json.endTime = this.endTime
 
-    console.log(json);
+    console.log(json)
 
-    return json;
+    return json
   }
 
   fromJSON(json) {
-    super.fromJSON(json);
+    super.fromJSON(json)
 
-    this.currentTime = json.currentTime;
-    this.endTime = json.endTime;
+    this.currentTime = json.currentTime
+    this.endTime = json.endTime
 
-    return this;
+    return this
   }
 }
 
-export { CustomEntity };
+export { CustomEntity }
--- examples/js/misc/savegame/src/CustomVehicle.js
+++ examples/js/misc/savegame/src/CustomVehicle.js
@@ -2,46 +2,46 @@
  * @author Mugen87 / https://github.com/Mugen87
  */
 
-import { Vehicle } from "../../../../build/yuka.module.js";
+import { Vehicle } from '../../../../../lib/yuka.module.js'
 
 class CustomVehicle extends Vehicle {
   constructor() {
-    super();
+    super()
 
-    this.name = "vehicle";
-    this.target = null;
+    this.name = 'vehicle'
+    this.target = null
   }
 
   update(delta) {
-    const seekBehavior = this.steering.behaviors[0];
-    seekBehavior.target.copy(this.target.position);
+    const seekBehavior = this.steering.behaviors[0]
+    seekBehavior.target.copy(this.target.position)
 
-    return super.update(delta);
+    return super.update(delta)
   }
 
   toJSON() {
-    const json = super.toJSON();
+    const json = super.toJSON()
 
-    json.target = this.target.uuid;
+    json.target = this.target.uuid
 
-    return json;
+    return json
   }
 
   fromJSON(json) {
-    super.fromJSON(json);
+    super.fromJSON(json)
 
-    this.target = json.target;
+    this.target = json.target
 
-    return this;
+    return this
   }
 
   resolveReferences(entities) {
-    super.resolveReferences(entities);
+    super.resolveReferences(entities)
 
-    this.target = entities.get(this.target);
+    this.target = entities.get(this.target)
 
-    return this;
+    return this
   }
 }
 
-export { CustomVehicle };
+export { CustomVehicle }
