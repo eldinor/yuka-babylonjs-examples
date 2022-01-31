abf30e4a709249ba190610ed257dc0f9eb52b63e
Roland Csibrei <roland.csibrei@nascor.tech>
2022-01-31 06:48:31 +0100

    Navmesh onwindowresize fix

--- examples/js/navigation/firstperson/src/FirstPersonControls.js
+++ examples/js/navigation/firstperson/src/FirstPersonControls.js
@@ -2,239 +2,203 @@
  * @author Mugen87 / https://github.com/Mugen87
  */
 
-import { EventDispatcher, Vector3, Logger } from '../../../../build/yuka.module.js';
+import { EventDispatcher, Vector3, Logger } from '../../../../../lib/yuka.module.js'
 
-const PI05 = Math.PI / 2;
-const direction = new Vector3();
-const velocity = new Vector3();
+const PI05 = Math.PI / 2
+const direction = new Vector3()
+const velocity = new Vector3()
 
-let currentSign = 1;
-let elapsedTime = 0;
+let currentSign = 1
+let elapsedTime = 0
 
 class FirstPersonControls extends EventDispatcher {
+  constructor(owner = null) {
+    super()
 
-	constructor( owner = null ) {
+    this.owner = owner
 
-		super();
+    this.movementX = 0 // mouse left/right
+    this.movementY = 0 // mouse up/down
 
-		this.owner = owner;
+    this.acceleration = 40
+    this.brakingPower = 10
+    this.lookingSpeed = 1
+    this.headMovement = 1.5
 
-		this.movementX = 0; // mouse left/right
-		this.movementY = 0; // mouse up/down
+    this.input = {
+      forward: false,
+      backward: false,
+      right: false,
+      left: false,
+    }
 
-		this.acceleration = 40;
-		this.brakingPower = 10;
-		this.lookingSpeed = 1;
-		this.headMovement = 1.5;
+    this.sounds = new Map()
 
-		this.input = {
-			forward: false,
-			backward: false,
-			right: false,
-			left: false
-		};
+    this._mouseMoveHandler = onMouseMove.bind(this)
+    this._pointerlockChangeHandler = onPointerlockChange.bind(this)
+    this._pointerlockErrorHandler = onPointerlockError.bind(this)
+    this._keyDownHandler = onKeyDown.bind(this)
+    this._keyUpHandler = onKeyUp.bind(this)
+  }
 
-		this.sounds = new Map();
+  connect() {
+    document.addEventListener('mousemove', this._mouseMoveHandler, false)
+    document.addEventListener('pointerlockchange', this._pointerlockChangeHandler, false)
+    document.addEventListener('pointerlockerror', this._pointerlockErrorHandler, false)
+    document.addEventListener('keydown', this._keyDownHandler, false)
+    document.addEventListener('keyup', this._keyUpHandler, false)
 
-		this._mouseMoveHandler = onMouseMove.bind( this );
-		this._pointerlockChangeHandler = onPointerlockChange.bind( this );
-		this._pointerlockErrorHandler = onPointerlockError.bind( this );
-		this._keyDownHandler = onKeyDown.bind( this );
-		this._keyUpHandler = onKeyUp.bind( this );
+    document.body.requestPointerLock()
+  }
 
-	}
+  disconnect() {
+    document.removeEventListener('mousemove', this._mouseMoveHandler, false)
+    document.removeEventListener('pointerlockchange', this._pointerlockChangeHandler, false)
+    document.removeEventListener('pointerlockerror', this._pointerlockErrorHandler, false)
+    document.removeEventListener('keydown', this._keyDownHandler, false)
+    document.removeEventListener('keyup', this._keyUpHandler, false)
+  }
 
-	connect() {
+  update(delta) {
+    const input = this.input
+    const owner = this.owner
 
-		document.addEventListener( 'mousemove', this._mouseMoveHandler, false );
-		document.addEventListener( 'pointerlockchange', this._pointerlockChangeHandler, false );
-		document.addEventListener( 'pointerlockerror', this._pointerlockErrorHandler, false );
-		document.addEventListener( 'keydown', this._keyDownHandler, false );
-		document.addEventListener( 'keyup', this._keyUpHandler, false );
+    velocity.x -= velocity.x * this.brakingPower * delta
+    velocity.z -= velocity.z * this.brakingPower * delta
 
-		document.body.requestPointerLock();
+    direction.z = Number(input.forward) - Number(input.backward)
+    direction.x = Number(input.left) - Number(input.right)
+    direction.normalize()
 
-	}
+    if (input.forward || input.backward) velocity.z -= direction.z * this.acceleration * delta
+    if (input.left || input.right) velocity.x -= direction.x * this.acceleration * delta
 
-	disconnect() {
+    owner.velocity.copy(velocity).applyRotation(owner.rotation)
 
-		document.removeEventListener( 'mousemove', this._mouseMoveHandler, false );
-		document.removeEventListener( 'pointerlockchange', this._pointerlockChangeHandler, false );
-		document.removeEventListener( 'pointerlockerror', this._pointerlockErrorHandler, false );
-		document.removeEventListener( 'keydown', this._keyDownHandler, false );
-		document.removeEventListener( 'keyup', this._keyUpHandler, false );
+    //
 
-	}
+    this._updateHead(delta)
+  }
 
-	update( delta ) {
+  setRotation(yaw, pitch) {
+    this.movementX = yaw
+    this.movementY = pitch
 
-		const input = this.input;
-		const owner = this.owner;
+    this.owner.rotation.fromEuler(0, this.movementX, 0)
+    this.owner.head.rotation.fromEuler(this.movementY, 0, 0)
+  }
 
-		velocity.x -= velocity.x * this.brakingPower * delta;
-		velocity.z -= velocity.z * this.brakingPower * delta;
+  _updateHead(delta) {
+    const owner = this.owner
+    const head = owner.head
 
-		direction.z = Number( input.forward ) - Number( input.backward );
-		direction.x = Number( input.left ) - Number( input.right );
-		direction.normalize();
+    // some simple head bobbing
 
-		if ( input.forward || input.backward ) velocity.z -= direction.z * this.acceleration * delta;
-		if ( input.left || input.right ) velocity.x -= direction.x * this.acceleration * delta;
+    const speed = owner.getSpeed()
 
-		owner.velocity.copy( velocity ).applyRotation( owner.rotation );
+    elapsedTime += delta * speed // scale delta with movement speed
 
-		//
+    const motion = Math.sin(elapsedTime * this.headMovement)
 
-		this._updateHead( delta );
+    head.position.y = Math.abs(motion) * 0.06
+    head.position.x = motion * 0.08
 
-	}
+    //
 
-	setRotation( yaw, pitch ) {
+    head.position.y += owner.height
 
-		this.movementX = yaw;
-		this.movementY = pitch;
+    //
 
-		this.owner.rotation.fromEuler( 0, this.movementX, 0 );
-		this.owner.head.rotation.fromEuler( this.movementY, 0, 0 );
+    const sign = Math.sign(Math.cos(elapsedTime * this.headMovement))
 
-	}
+    if (sign < currentSign) {
+      currentSign = sign
 
-	_updateHead( delta ) {
+      const audio = this.sounds.get('rightStep')
+      audio.play()
+    }
 
-		const owner = this.owner;
-		const head = owner.head;
+    if (sign > currentSign) {
+      currentSign = sign
 
-		// some simple head bobbing
-
-		const speed = owner.getSpeed();
-
-		elapsedTime += delta * speed; // scale delta with movement speed
-
-		const motion = Math.sin( elapsedTime * this.headMovement );
-
-		head.position.y = Math.abs( motion ) * 0.06;
-		head.position.x = motion * 0.08;
-
-		//
-
-		head.position.y += owner.height;
-
-		//
-
-		const sign = Math.sign( Math.cos( elapsedTime * this.headMovement ) );
-
-		if ( sign < currentSign ) {
-
-			currentSign = sign;
-
-			const audio = this.sounds.get( 'rightStep' );
-			audio.play();
-
-		}
-
-		if ( sign > currentSign ) {
-
-			currentSign = sign;
-
-			const audio = this.sounds.get( 'leftStep' );
-			audio.play();
-
-		}
-
-	}
-
+      const audio = this.sounds.get('leftStep')
+      audio.play()
+    }
+  }
 }
 
 // handler
 
-function onMouseMove( event ) {
+function onMouseMove(event) {
+  this.movementX -= event.movementX * 0.001 * this.lookingSpeed
+  this.movementY -= event.movementY * 0.001 * this.lookingSpeed
 
-	this.movementX -= event.movementX * 0.001 * this.lookingSpeed;
-	this.movementY -= event.movementY * 0.001 * this.lookingSpeed;
+  this.movementY = Math.max(-PI05, Math.min(PI05, this.movementY))
 
-	this.movementY = Math.max( - PI05, Math.min( PI05, this.movementY ) );
-
-	this.owner.rotation.fromEuler( 0, this.movementX, 0 ); // yaw
-	this.owner.head.rotation.fromEuler( this.movementY, 0, 0 ); // pitch
-
+  this.owner.rotation.fromEuler(0, this.movementX, 0) // yaw
+  this.owner.head.rotation.fromEuler(this.movementY, 0, 0) // pitch
 }
 
 function onPointerlockChange() {
+  if (document.pointerLockElement === document.body) {
+    this.dispatchEvent({ type: 'lock' })
+  } else {
+    this.disconnect()
 
-	if ( document.pointerLockElement === document.body ) {
-
-		this.dispatchEvent( { type: 'lock' } );
-
-	} else {
-
-		this.disconnect();
-
-		this.dispatchEvent( { type: 'unlock' } );
-
-	}
-
+    this.dispatchEvent({ type: 'unlock' })
+  }
 }
 
 function onPointerlockError() {
-
-	Logger.warn( 'YUKA.Player: Unable to use Pointer Lock API.' );
-
+  Logger.warn('YUKA.Player: Unable to use Pointer Lock API.')
 }
 
-function onKeyDown( event ) {
+function onKeyDown(event) {
+  switch (event.keyCode) {
+    case 38: // up
+    case 87: // w
+      this.input.forward = true
+      break
 
-	switch ( event.keyCode ) {
+    case 37: // left
+    case 65: // a
+      this.input.left = true
+      break
 
-		case 38: // up
-		case 87: // w
-			this.input.forward = true;
-			break;
+    case 40: // down
+    case 83: // s
+      this.input.backward = true
+      break
 
-		case 37: // left
-		case 65: // a
-			this.input.left = true;
-			break;
-
-		case 40: // down
-		case 83: // s
-			this.input.backward = true;
-			break;
-
-		case 39: // right
-		case 68: // d
-			this.input.right = true;
-			break;
-
-	}
-
+    case 39: // right
+    case 68: // d
+      this.input.right = true
+      break
+  }
 }
 
-function onKeyUp( event ) {
+function onKeyUp(event) {
+  switch (event.keyCode) {
+    case 38: // up
+    case 87: // w
+      this.input.forward = false
+      break
 
-	switch ( event.keyCode ) {
+    case 37: // left
+    case 65: // a
+      this.input.left = false
+      break
 
-		case 38: // up
-		case 87: // w
-			this.input.forward = false;
-			break;
+    case 40: // down
+    case 83: // s
+      this.input.backward = false
+      break
 
-		case 37: // left
-		case 65: // a
-			this.input.left = false;
-			break;
-
-		case 40: // down
-		case 83: // s
-			this.input.backward = false;
-			break;
-
-		case 39: // right
-		case 68: // d
-			this.input.right = false;
-			break;
-
-	}
-
+    case 39: // right
+    case 68: // d
+      this.input.right = false
+      break
+  }
 }
 
-export { FirstPersonControls };
+export { FirstPersonControls }
--- examples/js/navigation/firstperson/src/Player.js
+++ examples/js/navigation/firstperson/src/Player.js
@@ -2,55 +2,43 @@
  * @author Mugen87 / https://github.com/Mugen87
  */
 
-import { MovingEntity, GameEntity, Vector3 } from '../../../../build/yuka.module.js';
+import { MovingEntity, Vector3, GameEntity } from '../../../../../lib/yuka.module.js'
 
-const startPosition = new Vector3();
-const endPosition = new Vector3();
+const startPosition = new Vector3()
+const endPosition = new Vector3()
 
 class Player extends MovingEntity {
+  constructor() {
+    super()
 
-	constructor() {
+    this.maxSpeed = 4
+    this.height = 2
 
-		super();
+    this.head = new GameEntity()
+    this.add(this.head)
 
-		this.maxSpeed = 4;
-		this.height = 2;
+    this.updateOrientation = false
+    this.navMesh = null
+    this.currentRegion = null
+  }
 
-		this.head = new GameEntity();
-		this.add( this.head );
+  update(delta) {
+    startPosition.copy(this.position)
 
-		this.updateOrientation = false;
-		this.navMesh = null;
-		this.currentRegion = null;
+    super.update(delta)
 
-	}
+    endPosition.copy(this.position)
 
-	update( delta ) {
+    // ensure the entity stays inside its navmesh
 
-		startPosition.copy( this.position );
+    this.currentRegion = this.navMesh.clampMovement(this.currentRegion, startPosition, endPosition, this.position)
 
-		super.update( delta );
+    // adjust height of player according to the ground
 
-		endPosition.copy( this.position );
+    const distance = this.currentRegion.distanceToPoint(this.position)
 
-		// ensure the entity stays inside its navmesh
-
-		this.currentRegion = this.navMesh.clampMovement(
-			this.currentRegion,
-			startPosition,
-			endPosition,
-			this.position
-		);
-
-		// adjust height of player according to the ground
-
-		const distance = this.currentRegion.distanceToPoint( this.position );
-
-		this.position.y -= distance * 0.2; // smooth transition
-
-	}
-
+    this.position.y -= distance * 0.2 // smooth transition
+  }
 }
 
-
-export { Player };
+export { Player }
