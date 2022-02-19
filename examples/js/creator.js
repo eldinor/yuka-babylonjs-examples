2d7be82debe3ddb5833572177f7432f5e34b55b0
Roland Csibrei <roland.csibrei@nascortech.com>
2022-02-19 17:41:32 +0100

    Mesh creator

--- examples/js/creator.js
+++ examples/js/creator.js
@@ -2,7 +2,8 @@ import 'https://preview.babylonjs.com/babylon.js'
 
 export const VehicleTypes = {
   cone: 0,
-  box: 1,
+  basicCone: 1,
+  box: 2,
   // etc
 }
 
@@ -19,6 +20,20 @@ options: {
 export const createVehicle = (scene, options) => {
   const vehicleType = options?.vehicleType ?? VehicleTypes.cone
   if (vehicleType === VehicleTypes.cone) {
+    const height = 1 * options?.size ?? 1
+    const diameterBottom = 0.5 * options?.size ?? 1
+    const diameterTop = 0
+    const name = options?.name ?? 'vehicle'
+
+    // add the bullet here
+
+    // const vehicleMesh = BABYLON.MeshBuilder.CreateCylinder(name, { height, diameterTop, diameterBottom }, scene)
+
+    vehicleMesh.rotation.x = Math.PI * 0.5
+    vehicleMesh.bakeCurrentTransformIntoVertices()
+
+    return vehicleMesh
+  } else if (vehicleType === VehicleTypes.basicCone) {
     const height = 1 * options?.size ?? 1
     const diameterBottom = 0.5 * options?.size ?? 1
     const diameterTop = 0
